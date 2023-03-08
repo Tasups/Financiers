b08ded3c1b701d03dd9ad56ff59857f0fad28be6
EC2 Default User <ec2-user@ip-172-31-4-179.us-east-2.compute.internal>
2023-03-08 21:32:51 +0000

    polygon apis listed for use

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import axios from 'axios'
+
 import './App.css';
 
+// Polygon API docs https://polygon.io/docs/stocks/getting-started
+
+// Gives you the entire list of tickers
+const tickers = "https://api.polygon.io/v3/reference/tickers?active=true&apiKey=t_MS1QLfZrJGkotOfuYiqC2643_qYUaa"
+const tickersWithSearch = "https://api.polygon.io/v3/reference/tickers?search=search_term&active=true&apiKey=t_MS1QLfZrJGkotOfuYiqC2643_qYUaa"
+
+// Ticker Details requires the ticker and the date in question
+const tickerDetails = "https://api.polygon.io/v3/reference/tickers/AFL?date=2023-03-07&apiKey=t_MS1QLfZrJGkotOfuYiqC2643_qYUaa"
+
+// Gives you the high, low, open, close, and volume of a certain date of a ticker
+const dailyOpenAndClose = "https://api.polygon.io/v1/open-close/AFL/2023-03-07?adjusted=false&apiKey=t_MS1QLfZrJGkotOfuYiqC2643_qYUaa"
+
+
 function App() {
+  const [data, setData] = useState()
+  
+  const getData = () => {
+     axios
+      .get("https://api.polygon.io/v3/reference/tickers?active=true&apiKey=t_MS1QLfZrJGkotOfuYiqC2643_qYUaa")
+      .then(res => {
+        setData(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getData}>GET DATA</button>
+      <button onClick={() => console.log(data)}>LOG DATA</button>
     </div>
   );
 }
